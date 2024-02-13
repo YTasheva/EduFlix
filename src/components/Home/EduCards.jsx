a3156a5597a22b74be3ec89b990d81f005566fc9
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-13 18:14:42 +0000

    Add new education cards to Home component

--- src/components/Home/EduCards.jsx
+++ src/components/Home/EduCards.jsx
@@ -39,4 +39,31 @@ const cards = [
         image: education_1,
         circleText: "DISCOVER MORE",
       },
-      
+      {
+        id: 5,
+        price: "20,00",
+        date: "16.08.24",
+        topic: "Lorem Ipsum",
+        image: education_1,
+        circleText: "90% match",
+      },
+      {
+        id: 6,
+        price: "10,00",
+        date: "18.05.24",
+        topic: "Lorem Ipsum",
+        image: education_1,
+        circleText: "95% match",
+      },
+
+    ];
+
+    export default function EduCards() {
+      return (
+        <article className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 place-items-center lg:gap-14 gap-4">
+          {cards.map((card) => (
+            <EduCard key={card.id} card={card} />
+          ))}
+        </article>
+      );
+    }
