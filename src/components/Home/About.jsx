975b437de1a56ddbf9dacc9b59e3a76c44f8da61
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-13 19:16:18 +0000

    Add discover cards to About component

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -1,12 +1,22 @@
-import React from  'react';
+import { discover_1, discover_2, discover_3 } from "../../Assets/Home";
+import Container from "../Components/Container";
+import TitleSection from "./Components/TitleSection";
 
+const cards = [
+  {
+    id: 1,
+    image: digital_concept_1,
+    title: "Videos",
+  },
+  {
+    id: 2,
+    image: digital_concept_2,
+    title: "Tutorials",
+  },
+  {
+    id: 3,
+    image: digital_concept_3,
+    title: "Live",
+  },
+];
 
-function About() {
-    return (
-        <div>
-            <h1>Team Bananas</h1>
-        </div>
-    )
-}
-
-export default About;
