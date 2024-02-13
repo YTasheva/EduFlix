eba29f529a76fac68dd7949a061b4eb6839bd603
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-13 18:26:48 +0000

    Add search bar component

---
+++ src/components/Home/SearchBar.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import random from "../Assets/Home/Images/quiz.jpg";
+
+export default function SearchBar() {
+  const [focusBox, setFocusBox] = useState(false);
+  const focusHandler = () => {
+    setFocusBox(!focusBox);
+  };
+
+  return (
+    <div className="relative z-40">
+      <input
+        type="text"
+        placeholder="Search by Funds, Consortium, category"
+        onFocus={focusHandler}
+        onBlur={focusHandler}
+        className="bg-white bg-opacity-50 focus:bg-opacity-100 border-[1px] border-white outline-none px-4 py-2 rounded-md sm:w-[500px] w-[300px] placeholder-white"
+      />
+      
