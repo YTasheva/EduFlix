200db6611d0b2bd9016866a29357644c42973c62
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-13 19:16:43 +0000

    Add AboutCard component to Home/About.jsx

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -20,3 +20,30 @@ const cards = [
   },
 ];
 
+const AboutCard = ({ card }) => {
+    return (
+      <div className="relative rounded-xl overflow-hidden cursor-pointer hover:scale-105 transition-transform">
+        <img src={card.image} alt="about_image" />
+        <div className="absolute bottom-10 capitalize left-10 text-white font-bold md:text-[50px] text-[40px]">
+          {card.title}
+        </div>
+      </div>
+    );
+  };
+  
+  export default function About() {
+    return (
+      <section className="my-14">
+        <Container>
+          <div>
+            <TitleSection title="about" />
+            <div className="grid lg:grid-cols-3 mt-8 sm:grid-cols-2 grid-cols-1 place-items-center lg:gap-14 gap-4">
+              {cards.map((card) => (
+                <AboutCard card={card} key={card.id} />
+              ))}
+            </div>
+          </div>
+        </Container>
+      </section>
+    );
+  }
