f950ee09a0e26ea589ee3a17296d262b44f25232
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-13 18:13:54 +0000

    Add education cards to Home component

---
+++ src/components/Home/EduCards.jsx
@@ -0,0 +1,24 @@
+import EduCard from "./EduCard";
+import {
+    education_1,
+} from "../../Assets/Home/Images";
+
+const cards = [
+    {
+      id: 1,
+      price: "free",
+      date: "12.03.24",
+      topic: "Front-End Web Development - React",
+      image: education_1,
+      circleText: "Discover your choice",
+    },
+    {
+        id: 2,
+        price: "free",
+        date: "14.05.24",
+        topic:
+          "AI Basics - Creating AI driven education for a better connected Europe.",
+        image: education_1,
+        circleText: "LOGIN TO WATCH",
+      },
+     
