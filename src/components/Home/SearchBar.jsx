d4158553309653d8508d782be66d59b094565699
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-14 01:24:44 +0000

    Add trending topics and projects to SearchBar component

--- src/components/Home/SearchBar.jsx
+++ src/components/Home/SearchBar.jsx
@@ -16,4 +16,21 @@ export default function SearchBar() {
         onBlur={focusHandler}
         className="bg-white bg-opacity-50 focus:bg-opacity-100 border-[1px] border-white outline-none px-4 py-2 rounded-md sm:w-[500px] w-[300px] placeholder-white"
       />
-      
+      {focusBox && (
+        <div className="bg-white px-4 py-4 absolute top-[2.4rem] left-0 w-full text-[12px] border-t-[1px] border-t-black search_drop_shadow rounded-br-md rounded-bl-md">
+          <h2 className="uppercase text-gray-400 font-bold">trending topics</h2>
+          <div className="my-4 flex items-center gap-2 flex-wrap">
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">
+              AI - Technology
+            </p>
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">Front-End Web Development</p>
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">Facial Recognition</p>
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">Cyber Security</p>
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">Law</p>
+            <p className="bg-[#F6F6F6] rounded-lg px-2 py-1">MIT</p>
+          </div>
+          <div>
+            <h2 className="uppercase text-gray-400 font-bold py-4">projects</h2>
+            <div className="grid grid-cols-2 gap-4">
+              <div className="flex items-center gap-2 flex-wrap border-[1px] rounded-md p-2 cursor-pointer hover:bg-gray-600 hover:text-white border-gray-500">
+                
