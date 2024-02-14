746b736f340660bbf32538181a6fe9039aba48fc
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-14 01:30:42 +0000

    Add login button and mobile menu icon to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,4 +41,24 @@ function Navbar() {
             </div>
           </div>
 
-          
+          <Link to="/">
+            <div className="hidden md:block hover:bg-button-primary px-4 py-1 rounded-xl">
+              Log In
+            </div>
+          </Link>
+          <div className="-mr-2 flex md:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+        </div>
+
+        
