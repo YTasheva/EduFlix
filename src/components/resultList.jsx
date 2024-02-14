5c87d51bac1119bf64d6e601e5bddcbf9665d138
Jennifer Parsons <admin@Jennifer.local>
2024-02-14 19:20:46 +0000

    added js

--- src/components/resultList.jsx
+++ src/components/resultList.jsx
@@ -4,9 +4,11 @@ function SearchResult(props) {
   return (
     <ul className="list-group">
       {props.results.map(result => (
+        <iframe id="videoPlayer">
         <li className="list-group-item" key={result.id}>
           <img alt={result.title} className="img-fluid" src={result.images.original.url} />
         </li>
+        </iframe>
       ))}
     </ul>
   );
