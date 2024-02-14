0eb55eeb4ee9d8b0eba63a7eb9abaf456d54f54e
JonRahman <jonrahman90@gmail.com>
2024-02-14 20:01:59 +0000

    API updated

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,5 +1,5 @@
 import logo from "./logo.jpg";
-import banners_concept from "./banners_concept.jpg";
+// import banners_concept from "./banners_concept.jpg";
 import digital_concept_1 from "./digital_concept_1.jpg";
 import digital_concept_2 from "./digital_concept_2.jpg";
 import digital_concept_3 from "./digital_concept_3.jpg";
@@ -16,7 +16,7 @@ import quiz from "./quiz.jpg";
 
 
 export {
-    banners_concept,
+//    banners_concept,
     digital_concept_1,
     digital_concept_2,
     digital_concept_3,
