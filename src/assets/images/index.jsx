795892a28ccf5a7bbdea6c955db779e0c3e76f07
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-14 18:09:29 +0000

    Add new education images

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -4,6 +4,14 @@ import digital_concept_1 from "./digital_concept_1.jpg";
 import digital_concept_2 from "./digital_concept_2.jpg";
 import digital_concept_3 from "./digital_concept_3.jpg";
 import education_1 from "./education_1.jpg";
+import education_2 from "./education_2.jpg";
+import education_3 from "./education_3.jpg";
+import education_4 from "./education_4.jpg";
+import education_5 from "./education_5.jpg";
+import education_6 from "./education_6.jpg";
+import education_7 from "./education_7.jpg";
+import education_8 from "./education_8.jpg";
+
 import quiz from "./quiz.jpg";
 
 
@@ -13,6 +21,13 @@ export {
     digital_concept_2,
     digital_concept_3,
     education_1,
+    education_2,
+    education_3,
+    education_4,
+    education_5,
+    education_6,
+    education_7,
+    education_8,
     quiz,
     logo,
     };
