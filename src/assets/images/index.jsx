32dcacc7fbc4810b97a5fc01ea1f3adc34b9ea3c
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-14 01:23:27 +0000

    Add image assets to index.jsx

---
+++ src/assets/images/index.jsx
@@ -0,0 +1,18 @@
+import logo from "./logo.jpg";
+import banners_concept from "./banners_concept.jpg";
+import digital_concept_1 from "./digital_concept_1.jpg";
+import digital_concept_2 from "./digital_concept_2.jpg";
+import digital_concept_3 from "./digital_concept_3.jpg";
+import education_1 from "./education_1.jpg";
+import quiz from "./quiz_bg.jpg";
+
+
+export {
+    banners_concept,
+    digital_concept_1,
+    digital_concept_2,
+    digital_concept_3,
+    education_1,
+    quiz,
+    logo,
+    };
