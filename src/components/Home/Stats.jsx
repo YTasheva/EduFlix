c76276cf644c8cab19c477ac0500a2d1b468483a
Yuliya Tasheva <148258557+YTasheva@users.noreply.github.com>
2024-02-15 00:55:29 +0000

    Update currency symbol and text in Stats component

--- src/components/Home/Stats.jsx
+++ src/components/Home/Stats.jsx
@@ -10,8 +10,8 @@ export default function Stats() {
           <p>Users</p>
         </div>
         <div>
-          <h1 className="md:text-[40px] text-[25px] font-bold">€200M</h1>
-          <p>Education Library</p>
+          <h1 className="md:text-[40px] text-[25px] font-bold">£200M</h1>
+          <p>Education Grants</p>
         </div>
         <div>
           <h1 className="md:text-[40px] text-[25px] font-bold">50+</h1>
